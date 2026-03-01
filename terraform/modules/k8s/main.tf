40c2a7d009d4a7b62277816870f49c8f32dd1ea9
CristoferJimenez1 <contact@cristoferjimenez.com>
2026-03-01 03:20:54 +0000

    fix: add gp3 StorageClass via Helm chart and EBS CSI driver via Terraform

--- terraform/modules/k8s/main.tf
+++ terraform/modules/k8s/main.tf
@@ -24,3 +24,37 @@ module "eks" {
     }
   }
 }
+
+resource "aws_iam_role" "ebs_csi_driver" {
+  name = "${var.cluster_name}-ebs-csi-driver"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = module.eks.oidc_provider_arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "${module.eks.oidc_provider}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+          "${module.eks.oidc_provider}:aud" = "sts.amazonaws.com"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_driver" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = aws_iam_role.ebs_csi_driver.name
+}
+
+resource "aws_eks_addon" "ebs_csi_driver" {
+  cluster_name = module.eks.cluster_name
+  addon_name   = "aws-ebs-csi-driver"
+  depends_on   = [module.eks, aws_iam_role_policy_attachment.ebs_csi_driver]
+
+  service_account_role_arn = aws_iam_role.ebs_csi_driver.arn
+}
